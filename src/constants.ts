6bcb9ff0adaa7f337eff85c83b42f62e0b7e3da7
Akshansh0712 <akshbhagat0712@gmail.com>
2026-02-15 01:12:57 +0530

    Updated app logo, added adaptive icons, completed currency converter with CurrencyButton component

---
+++ src/constants.ts
@@ -0,0 +1,56 @@
+export const currencyByRupee: Currency[] = [
+    {
+      name: 'DOLLAR',
+      value: 0.011171428,
+      flag: "🇺🇸",
+      symbol: '$',
+    },
+    {
+    name: "DIRHAM (UAE)",
+    value: 0.0405,
+    flag: "🇦🇪",
+    symbol: "د.إ",
+  },
+  {
+    name: "AUS DOLLAR",
+    value: 0.0157,
+    flag: "🇦🇺",
+    symbol: "A$",
+  },
+  {
+    name: "BRITISH POUND",
+    value: 0.00807,
+    flag: "🇬🇧",
+    symbol: "£",
+  },
+  {
+    name: "EURO",
+    value: 0.00935,
+    flag: "🇪🇺",
+    symbol: "€",
+  },
+  {
+    name: "SINGA DOLLAR",
+    value: 0.01406,
+    flag: "🇸🇬",
+    symbol: "S$",
+  },
+  {
+    name: 'YEN',
+    value: 1.7209089,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: "THAI BAHT",
+    value: 2.6,
+    flag: "🇹🇭",
+    symbol: "฿",
+  },
+  {
+    name: "BITCOIN",
+    value: 0.000000158,
+    flag: "🪙",
+    symbol: "BTC",
+  },
+];
